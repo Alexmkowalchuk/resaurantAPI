e19dbe047ab69ab9d5562e76b13070d771c1b299
Alex Kowalchuk <alexmkowalchuk@gmail.com>
2021-01-20 11:07:41 -0500

    updated

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: ___Alex Kowalchuk____ Student ID: ______________ Date: ______2021-01-20______
+*  Name: ___Alex Kowalchuk__ Student ID: ___021861158___ Date: ___2021-01-20_____
 *  Heroku Link: ______https://amk-web422-assignment1.herokuapp.com/________
 ********************************************************************************/ 
 
